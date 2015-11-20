2fea7adb386c5ce5c14b008dab67ca5acc5b3069
vahidsotudeh <vahidsotudeh@gmail.com>
2015-11-20 20:36:08 +0330

    hi this is first commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("first change");
         System.out.println("Hello World!");
     }
 }
